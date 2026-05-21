fc0c0dee668f2f3276082a838d7f7d91f7b3ab7a
Pranav <lasepranav2021@gmail.com>
2026-05-21 16:17:47 +0530

    Fix: remove duplicate axios import

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,24 +1,20 @@
 import axios from 'axios';
 
+const BASE = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}/api`
+  : '/api';
+
+const api = axios.create({ baseURL: BASE });
 
 export const getTasks = (params) => api.get('/tasks', { params });
 export const createTask = (data) => api.post('/tasks', data);
 export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
 export const deleteTask = (id) => api.delete(`/tasks/${id}`);
 export const getTaskStats = (projectId) => api.get(`/tasks/stats/${projectId}`);
-
 export const getProjects = () => api.get('/projects');
 export const createProject = (data) => api.post('/projects', data);
 export const seedData = () => api.post('/projects/seed');
-
 export const getUsers = () => api.get('/users');
 export const getActivities = (params) => api.get('/activities', { params });
 
 export default api;
-
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-    ? `${process.env.REACT_APP_API_URL}/api`
-    : '/api'
-});
